eb79354abbe15683140bd1018a98cac12d7f0460
Tehsmeely <jon.heiser@gmail.com>
2016-09-28 01:41:39 +0100

    Clean up main file

--- threadQueueProvider.py
+++ threadQueueProvider.py
@@ -1,7 +1,5 @@
 import threading, Queue, random, time
 
-random.seed()
-
 class ThreadQueueProvider(threading.Thread):
 	def __init__(self, maxSize=1):
 		threading.Thread.__init__(self)
@@ -50,43 +48,3 @@ class ThreadQueueProvider(threading.Thread):
 		rn = random.randint(0, 100)
 		print "Provider: genning random number: ", rn
 		return rn
-
-
-# class Getter(threading.Thread):
-# 	def __init__(self, provider):
-# 		threading.Thread.__init__(self)
-# 		self.provider = provider
-# 		self._isRunning = False
-
-# 	def run(self):
-# 		print "Getter: starting"
-# 		self._isRunning = True
-# 		self._loop()
-
-# 	def _loop(self):
-# 		while self._isRunning:
-# 			time.sleep(5)
-# 			print "Getter: getting val: {}".format(provider.queue.get())
-
-
-
-# if __name__ == "__main__":
-# 	provider = Provider(maxSize=5)
-# 	getter = Getter(provider)
-
-
-# 	provider.start()
-# 	getter.start()
-
-
-# 	while True:
-# 		try:
-# 			inpt = raw_input()
-# 			if inpt == "x":
-# 				break
-# 		except Exception as e:
-# 			print e
-# 			break
-
-# 	provider._isRunning = False
-# 	getter._isRunning = False
